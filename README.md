4745fc1ac42cb17c79fe03f25d2f63729609b2ad
Abhinav-175 <60563358+Abhinav-175@users.noreply.github.com>
2025-10-26 23:18:12 +0000

    init + readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # hpdf
 Highlight pdf documents and papers.
+
+### Install
+
+Install and use easily with [uv](https://docs.astral.sh/uv/):
+
+```bash
+uv tool install git+https://github.com/Abhinav-175/hpdf.git
+hpdf test.pdf
+```
+
+**hpdf** is a not so smart plumbing between Pymupdf and a singular GEMINI api call.
+Works by extracting text from the docment and ignoring images.
+So, does'nt work on rasterized pdf documents.
+
+Bring your own keys approach.
+
+```bash
+export GEMINI_API_KEY="0000...000"
+```
+
+Notes:
+- No safety checks on the size of the document, be mindful.
+- The behaviour around password protected documents falls on Pymupdf's defaults.
+- The "smartness" or the lack there of is inherited from GEMINI.
+
+### License
+
+[AGPL](https://www.gnu.org/licenses/agpl-3.0.html)
---
+++ hpdf.py
@@ -0,0 +1,109 @@
+"""
+License : AGPL <https://www.gnu.org/licenses/agpl-3.0-standalone.html>
+"""
+
+import os
+import pathlib
+import requests
+import json
+import argparse
+import sys
+import itertools
+import time
+import re
+
+# external libs
+import fitz
+
+
+gemini_pro_sys_prompt = """\
+Analyze the following document text. Identify and return only the **exact** sentences or paragraphs that represent the core thesis or most critical findings. Do not paraphrase or add any introductory text. Output atleast 25 line extracts.\n\n\
+"""
+
+
+def get_gemini_api_key()->str:
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key:
+        return api_key
+    else:
+        raise RuntimeError("Could not find Gemini api key\n export GEMINI_API_KEY='0000...000'")
+
+
+def gemini_api_call(sys_prompt, pdf_text_concat):
+    api_key = get_gemini_api_key()
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent"
+    headers = {
+        "x-goog-api-key": api_key,
+        "Content-Type": "application/json"
+    }
+    data = {"contents": [{"parts": [{"text": f"{sys_prompt+pdf_text_concat}"}]}]}
+
+    print("Calling Gemini to process document....")
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    # response clean-up
+    try:
+        response_json = response.json()
+        extracted_text = response_json.get("candidates", [{}])[0].get("content", {}).get("parts", [{}])[0].get("text")
+        
+        if extracted_text is not None:
+            return extracted_text
+        else:
+            print("Could not extract text from response.")
+    
+    except requests.exceptions.JSONDecodeError:
+        print("Error: Response is not valid JSON.")
+    except IndexError:
+        print("Error: List index out of range in response.")
+
+
+def resp_split(extracted_text):
+    output_strings = []
+    response_split = re.split(r"[^\(\)\ﬁ\,\-a-zA-Z0-9\s]+", extracted_text)
+    for i in response_split:
+        if len(i)>15:       # to drop unexpected splits
+            output_strings.append(i)
+    
+    return output_strings
+
+
+def process_pdf(pdf_path):
+    basename = os.path.basename(pdf_path)
+    filename_stem = os.path.splitext(basename)[0]
+    output_file = "./highlighted_"+filename_stem+".pdf"
+
+    doc = fitz.open(pdf_path)
+    pages = [i for i in doc]
+    pages_text = []
+    
+    print("Processing document")
+    for page in pages: pages_text.append(page.get_text())
+
+    # main api call
+    search_terms = resp_split(gemini_api_call(gemini_pro_sys_prompt, ''.join(pages_text)))
+
+    for page in pages:
+        for term in search_terms:
+            search_results = page.search_for(term)
+            for hits in search_results:
+                highlight = page.add_highlight_annot(hits)
+    
+    print(f"Write output to {output_file}")
+    doc.save(output_file)
+    doc.close()
+
+
+def main()->int:
+    ap = argparse.ArgumentParser(description="Highlight core concepts in a paper.\nExpectes env variable GEMINI_API_KEY:\nexport GEMINI_API_KEY='0000...000'")
+    ap.add_argument("pdf_doc", type=str, help="path to pdf to be processed.")
+    args = ap.parse_args()
+
+    if not args.pdf_doc.lower().endswith(".pdf"):
+        ap.error(f"file '{args.pdf_doc}' must be a .pdf document.")
+    
+    process_pdf(args.pdf_doc)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
